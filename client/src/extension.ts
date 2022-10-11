0fa9ddabd2f4a93be90d0e5b0d420252f88e8ee8
Viijay-Kr <vjkrishna689@gmail.com>
2022-10-11 19:39:04 +0200

    perf(1.0.0): #2 optimization using a storage layer

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,15 +1,12 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
 'use strict';
 
 import { ExtensionContext, window, workspace, languages } from 'vscode';
-import * as fsg from 'fast-glob';
 import { definitionProvider } from './providers/definitions';
 import { hoverProvider } from './providers/hover';
 import { completetionProvider } from './providers/completion';
 import { Settings } from './settings';
+import Storage from './storage/Storage';
+
 const documentSelector = [
 	{ scheme: 'file', language: 'typescriptreact' },
 	{ scheme: 'file', language: 'typescript' }
@@ -19,43 +16,46 @@ let workSpaceFiles: string[];
 const settings = new Settings();
 
 workspace.onDidCreateFiles((e) => {
-	const files = e.files;
-	files.forEach((f) => {
-		if (f.path.endsWith('.css') || f.path.endsWith('.scss')) {
-			workSpaceFiles.push(f.path);
-		}
-	});
+	Storage.addSourceFiles(e.files);
 });
 
-export async function activate(context: ExtensionContext): Promise<void> {
-	const uri = window.activeTextEditor?.document?.uri;
-	if (uri) {
-		const workspaceRoot = workspace.getWorkspaceFolder(uri)?.uri.fsPath;
-		workSpaceFiles = await fsg('**/*.{scss,css}', {
-			cwd: workspaceRoot,
-			ignore: ['node_modules', 'build'],
-			absolute: true,
-		});
-	}
-	const _definitionProvider = languages.registerDefinitionProvider(documentSelector, definitionProvider({ files: workSpaceFiles }));
-	const _hoverProvider = languages.registerHoverProvider(documentSelector, hoverProvider({ files: workSpaceFiles }));
-	const _completionProvider = languages.registerCompletionItemProvider(
-		documentSelector,
-		completetionProvider({
-			files: workSpaceFiles
-		}),
-		'.', '\'', '\[');
-	if (settings.autoComplete) {
-		context.subscriptions.push(_completionProvider);
-	}
-	if (settings.definition) {
-		context.subscriptions.push(_definitionProvider);
-	}
-	if (settings.references) {
-		context.subscriptions.push(_hoverProvider);
-	}
+workspace.onDidOpenTextDocument(() => {
+	Storage.bootStrap();
+});
 
+workspace.onDidChangeTextDocument(() => {
+	Storage.bootStrap();
+});
+
+
+
+export async function activate(context: ExtensionContext): Promise<void> {
+	try {
+		Storage.bootStrap();
+		const _definitionProvider = languages.registerDefinitionProvider(documentSelector, definitionProvider({ files: workSpaceFiles }));
+		const _hoverProvider = languages.registerHoverProvider(documentSelector, hoverProvider({ files: workSpaceFiles }));
+		const _completionProvider = languages.registerCompletionItemProvider(
+			documentSelector,
+			completetionProvider({
+				files: workSpaceFiles
+			}),
+			'.', '\'', '\[');
+
+		if (settings.autoComplete) {
+			context.subscriptions.push(_completionProvider);
+		}
+		if (settings.definition) {
+			context.subscriptions.push(_definitionProvider);
+		}
+		if (settings.references) {
+			context.subscriptions.push(_hoverProvider);
+		}
+		window.showInformationMessage('React-TS-CSS activated successfully');
+	} catch (e) {
+		window.showWarningMessage('Something went wrong while activating React-TS-CSS extension');
+	}
 }
 
 export function deactivate() {
+	Storage.clear();
 }
---
+++ client/src/providers/ProviderFactory.ts
@@ -0,0 +1,145 @@
+import { Position, Range } from 'vscode';
+import Storage from '../storage/Storage';
+import * as fs from 'fs/promises';
+import { filterChildSelector, filterParentSelector, getSymbolContent, parseCss, scssSymbolMatcher } from '../parser/css';
+import { SymbolInformation } from 'vscode-css-languageservice';
+import { ImportDeclaration } from '@babel/types';
+export enum ProviderKind {
+	Definition = 1,
+	Completion = 2,
+	Hover = 3,
+	Invalid = -1
+}
+export class ProviderFactory {
+	/** Dynamic CSS file which should be parsed for completion,definition and hover */
+	public sourceCssFile: string | undefined;
+	public providerKind: ProviderKind = ProviderKind.Invalid;
+	public position: Position;
+	// Source css file content
+	private sourceCssContent: string = '';
+	public constructor(options: { providerKind: ProviderKind, position: Position }) {
+		this.providerKind = options.providerKind;
+		this.position = options.position;
+		this.setSourceCssFile(options.position);
+	}
+
+	/**
+	 * Set the source css file for processing provider results. The file is determined by the current position in the TextDocument and finding the available css import declarations in the Document
+	 * @param position [Position](#vscode.Position);
+	 */
+	public setSourceCssFile(position: Position) {
+		const document = Storage.getActiveTextDocument();
+		const nodeAtOffset = Storage.getNodeAtOffsetPosition(document.offsetAt(position));
+		const nodeByFile = Storage.getNodeByFile();
+		const targetIdentifier = nodeAtOffset?.parent;
+		const targetImport = nodeByFile?.sourceIdentifiers?.find(s => s.identifier.name === targetIdentifier?.name)?.import;
+		if (targetImport) {
+			const files = Array.from(Storage.sourceFiles.keys());
+			this.sourceCssFile = files.find(f => f.includes(targetImport.source.value.split('/').pop()!));
+		}
+	}
+
+	public async getAllSymbols() {
+		if (!this.sourceCssFile) {
+			throw new Error('No source css file uri found. Did you create a ProviderFactory instance');
+		}
+		const symbols = Storage.getCSSSymbols(this.sourceCssFile);
+		if (!symbols) {
+			// If not found in cache get it from a fresh parse
+			const fileContent = await fs.readFile(this.sourceCssFile);
+			this.sourceCssContent = fileContent.toString();
+			const symbols = parseCss(this.sourceCssFile, fileContent.toString());
+			Storage.setCssSymbols(this.sourceCssFile, symbols);
+			return symbols;
+		}
+		return symbols;
+	}
+
+	public async setSourceCssFileContents() {
+		if (!this.sourceCssFile) {
+			throw new Error('No Css file found');
+		}
+		if (this.sourceCssContent) { return; }
+		this.sourceCssContent = (await fs.readFile(this.sourceCssFile)).toString();
+	}
+
+	public async getMatchedSelectors() {
+		const symbols = await this.getAllSymbols();
+		const document = Storage.getActiveTextDocument();
+		const nodeAtOffset = Storage.getNodeAtOffsetPosition(document.offsetAt(this.position));
+		if (!nodeAtOffset?.literal.value) {
+			return;
+		}
+		return scssSymbolMatcher(symbols, nodeAtOffset?.literal.value);
+	}
+
+	public async getAllSelectors() {
+		const symbols = await this.getAllSymbols();
+		const parentSelectors = symbols.filter(filterParentSelector);
+		const childSelectors = parentSelectors.filter(filterChildSelector);
+		return {
+			parentSelectors,
+			childSelectors,
+		};
+	}
+	public getOriginWordRange() {
+		const document = Storage.getActiveTextDocument();
+		const nodeAtOffset = Storage.getNodeAtOffsetPosition(document.offsetAt(this.position));
+		if (!nodeAtOffset) {
+			return;
+		}
+		return new Range(
+			new Position(
+				nodeAtOffset.parent.loc?.start.line! - 1,
+				nodeAtOffset.parent.loc?.start.column!
+			),
+			new Position(
+				nodeAtOffset.literal.loc?.end.line! - 1,
+				nodeAtOffset.literal.loc?.end.column!
+			)
+		);
+	}
+
+	public getSymbolLocationRange(symbol: SymbolInformation) {
+		return new Range(
+			new Position(symbol.location.range.start.line, symbol.location.range.start.character),
+			new Position(symbol.location.range.end.line, symbol.location.range.end.character)
+		);
+	}
+
+	public getSymbolContent(symbol: SymbolInformation) {
+		if (!this.sourceCssFile) {
+			throw new Error('No source css file uri found. Did you create a ProviderFactory instance');
+		}
+		return getSymbolContent(symbol, this.sourceCssContent);
+	}
+
+	public preProcessCompletions() {
+		const currentRange = new Range(
+			new Position(this.position.line, this.position.character),
+			new Position(this.position.line, this.position.character)
+		);
+		const document = Storage.getActiveTextDocument();
+		const nodeByFile = Storage.getNodeByFile();
+		let targetDeclration: ImportDeclaration;
+		nodeByFile?.sourceIdentifiers.forEach((i) => {
+			if (targetDeclration) { return; }
+			const identifier = i.identifier.name;
+			const wordToMatch = document.getText(
+				currentRange.with(
+					new Position(this.position.line, this.position.character - identifier.length - 1),
+					new Position(this.position.line, this.position.character - 1)
+				)
+			);
+			const match = wordToMatch.match(new RegExp(i.identifier.name));
+			if (match) {
+				targetDeclration = i.import;
+
+			}
+		});
+		const files = Array.from(Storage.sourceFiles.keys());
+		this.sourceCssFile = files.find(f => {
+			return f.includes(targetDeclration?.source.value.split('/').pop()!);
+		});
+	}
+}
