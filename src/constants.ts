008965d56f52cbafc89b59ad72b4f7406226bd99
Viijay-Kr <vjkrishna689@gmail.com>
2023-08-30 15:28:19 +0200

    feat: bringing back code lens and reference providers

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const CSS_MODULE_EXTENSIONS = [".css", ".scss", ".less"] as const;
 
-export type CssModuleExtensions = typeof CSS_MODULE_EXTENSIONS[number];
+export type CssModuleExtensions = (typeof CSS_MODULE_EXTENSIONS)[number];
 
-export const MODULE_EXTENSIONS = [".tsx", ".ts", ".jsx", ".js"];
+export const TS_MODULE_EXTENSIONS = [".tsx", ".ts", ".jsx", ".js"];
 
 export const colors: { [name: string]: string } = {
   aliceblue: "#f0f8ff",
